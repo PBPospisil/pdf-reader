d2b7cdc075a0aa364d598a93ea303197229315a2
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-06-24 17:19:34 -0700

    eslint config change

--- phoenix/.eslintrc.js
+++ phoenix/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  plugins: ['@typescript-eslint'],
 };
