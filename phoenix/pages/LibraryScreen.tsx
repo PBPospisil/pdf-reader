3bcdd4c2327ddc1233ef6ca6b08992d5b6a30bb4
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-06-30 13:59:00 -0700

    create library comp and library stylesheet

--- phoenix/pages/LibraryScreen.tsx
+++ phoenix/pages/LibraryScreen.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { View } from 'react-native';
+import { LibraryStyles } from '../ui/styles';
 
 const LibraryScreen = () => {
     return (
         <View></View>
     );
 }
-
-export default LibraryScreen;
---
+++ phoenix/ui/styles.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+export const LibraryStyles = StyleSheet.create({
+    screen: {
+        flex: 1
+    }
+})
