c37d92bccd4c91e4837ef812a4c4e8f6f38a719a
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-07-01 16:34:10 -0700

    state and empty flat list

--- phoenix/components/Shelf.tsx
+++ phoenix/components/Shelf.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FlatList, Text, View } from 'react-native';
 
 const Shelf = () => {
+    const [books, setBooks] = useState([]);
+
     return (
-        <></>
+        <View>
+            <FlatList
+            data={[]}
+            renderItem={() => <Text></Text>}
+            />
+        </View>
     );
 }
 
