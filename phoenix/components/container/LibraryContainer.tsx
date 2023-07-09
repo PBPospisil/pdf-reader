3bd52f259a6b818d3d816caf5d656f5612d29b10
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-07-08 19:58:27 -0700

    split library into presenter and container

---
+++ phoenix/components/container/LibraryContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { LibraryPresenter } from "../presenter/LibraryPresenter"
+
+const LibraryContainer = () =>
+{
+    return (
+        <LibraryPresenter></LibraryPresenter>
+    )
+}
