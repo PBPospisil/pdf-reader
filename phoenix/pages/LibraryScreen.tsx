0eb6c4efcd03c152dfb69b404e7c91b2a4a335d4
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-07-11 14:30:19 -0700

    implemented header in library screen

--- phoenix/pages/LibraryScreen.tsx
+++ phoenix/pages/LibraryScreen.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { LibraryContainer } from '../components/container/LibraryContainer'
+import { LibraryHeader } from '../components/LibraryHeader'
+import { View } from 'react-native'
+import { LibraryStyles } from '../ui/styles'
 
 const LibraryScreen = () => {
     return (
-        <LibraryContainer></LibraryContainer>
+        <View>
+            <LibraryHeader></LibraryHeader>
+            <LibraryContainer></LibraryContainer>
+        </View>
+        
     );
 }
 
