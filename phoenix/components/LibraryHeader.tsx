0ce65eea4aac0af5a6745963097766e9c4601168
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-07-11 14:18:41 -0700

    basic library header component

---
+++ phoenix/components/LibraryHeader.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Text } from "react-native"
+import { LibraryStyles } from "../ui/styles"
+
+export const LibraryHeader = () =>
+{
+    return (
+        <Text>Library</Text>
+    )
+}
