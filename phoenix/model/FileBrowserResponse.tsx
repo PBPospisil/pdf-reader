fd1aca06668fd4db865449b267e9ca03c3716315
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-07-13 21:44:28 -0700

    file browser tests

--- phoenix/model/FileBrowserResponse.tsx
+++ phoenix/model/FileBrowserResponse.tsx
@@ -7,15 +7,18 @@ export type FileBrowserResponse = {
     isCancelled?: Boolean
 }
 
-export function isSuccess(res: FileBrowserResponse)
+export function isSuccess(res: FileBrowserResponse): Boolean
 {
+    if(res.error !== undefined) {
+        return false
+    }
     if(Array.isArray(res.response) && !isEmpty(res.response)) {
         return true
     }
     return false
 }
 
-export function isFailure(res: FileBrowserResponse)
+export function isFailure(res: FileBrowserResponse): Boolean
 {
     return !isSuccess(res)
 }
