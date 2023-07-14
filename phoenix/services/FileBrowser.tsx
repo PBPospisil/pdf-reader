7a6ef3a07aead4de581d2661465b4a81fbd619e4
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-07-13 19:51:06 -0700

    file browser service

---
+++ phoenix/services/FileBrowser.tsx
@@ -0,0 +1,19 @@
+import { FileBrowserResponse } from '../model/FileBrowserResponse'
+import { pickDocument, isCancelled } from './DocumentPickerService'
+
+export type UNEXPECTED_EXECUTION_FLOW_ERROR = {}
+
+export async function browseFile(): Promise<FileBrowserResponse>
+{
+    pickDocument()
+        .then(function (success) {
+            return { response: success, error: '', isCancelled: false } as FileBrowserResponse
+        })
+        .catch(function (error) {
+            if(!isCancelled(error)) {
+                // TODO: TOAST GENERIC ERROR MESSAGE
+                return { response: [], error: error, isCancelled: false } as FileBrowserResponse
+            }
+        })
+    return { response: [], error: Error as UNEXPECTED_EXECUTION_FLOW_ERROR, isCancelled: false } as FileBrowserResponse
+}
