b5012814aea94e1aec1a643197199d97968b9855
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-07-15 18:58:29 -0700

    hook up middleware to store config

--- app/store.ts
+++ app/store.ts
@@ -1,15 +1,24 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import { configureStore, ThunkDispatch, Action } from "@reduxjs/toolkit"
 import ShelfReducer from "../src/features/shelf/ShelfSlice"
-import FileBrowserSlice from "../src/features/files/FileBrowserSlice"
+import FileBrowserReducer from "../src/features/files/FileBrowserSlice"
+import { addBookToLibrary } from "../src/middleware/AddBookToLibrary"
+import { fetchFile } from "../src/middleware/FetchFile"
 
 const store = configureStore({
     reducer: {
         shelf: ShelfReducer,
-        fileBrowser: FileBrowserSlice
-    }    
+        fileBrowser: FileBrowserReducer
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: { addBookToLibrary, fetchFile },
+      },
+      serializableCheck: false,
+    })
 })
 
 export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = ThunkDispatch<RootState, void, Action>;
 
 export default store
