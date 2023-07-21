55fbb41bb463f85cca7f340b8df1521e00923195
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-07-21 15:36:07 -0700

    flatlist impl

--- src/features/shelf/ShelfView.tsx
+++ src/features/shelf/ShelfView.tsx
@@ -1,19 +1,41 @@
-import React, { useState } from 'react'
-import { Book } from '../../types';
-import { View, FlatList, Text } from "react-native"
-import { useBooks } from '../../hooks/hooks';
-import { BookView } from '../book/BookView';
+import React from 'react'
+import { Book } from '../../types'
+import { View, FlatList } from "react-native"
+import { useBooks } from '../../hooks/hooks'
+import { BookView } from '../book/BookView'
+import AddBookButton from '../../components/AddBookButton'
+import { ShelfStyles } from '../../ui/styles'
 
-const ShelfView = () => 
-{
-    const books = useBooks()
-    
+interface itemProps {
+    item: Book
+}
+
+const renderItem = ({ item }: itemProps) => {
     return (
-        <View>
-            <FlatList
-                data={[]}
-                renderItem={() => <Text></Text>}
-            />
+        <BookView book={{
+            title: item.title,
+            cover: item.cover,
+            author: item.author,
+            publicationDate: item.publicationDate,
+            path: item.path,
+            fileType: item.fileType
+        }}
+        />
+    )
+}
+
+const ShelfView = () => {
+    const books = useBooks()
+
+    return (
+        <View style={ShelfStyles.container}>
+            <View style={ShelfStyles.list}>
+                <FlatList
+                    data={books}
+                    renderItem={renderItem}
+                />
+            </View>
+            <AddBookButton></AddBookButton>
         </View>
     )
 }
