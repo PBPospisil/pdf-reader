c451ac56a33ba82afa1084d5b7152d957402c6cf
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-07-25 15:06:17 -0700

    reader screen

---
+++ src/pages/ReaderScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { SafeAreaView, useWindowDimensions } from 'react-native'
+
+const ReaderScreen = () => {
+
+    return (
+        <SafeAreaView>
+        </SafeAreaView>
+    )
+}
+
+export default ReaderScreen
