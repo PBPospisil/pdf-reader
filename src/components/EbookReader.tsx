3a8801392286c38054eefef0aa23be63adae7433
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-10-04 20:00:38 -0600

    pdf reader basic impl

--- src/components/EbookReader.tsx
+++ src/components/EbookReader.tsx
@@ -1,20 +1,31 @@
 import React from "react"
-import { FileType } from "../types"
+import { Book, FileType } from "../types"
 import EpubReader from "./EpubReader"
 import PdfReader from "./PdfReader"
 
+var RNFS = require('react-native-fs');
+
 type EbookProps = {
-    fileType: FileType;
+    book: Book;
+}
+
+const determineReaderType = (props: EbookProps) =>
+{
+    if(props.book.fileType === FileType.epub) {
+        return <EpubReader></EpubReader>
+    }
+    if(props.book.fileType === FileType.pdf) {
+        return <PdfReader book={props.book}></PdfReader>
+    }
 }
 
 const EbookReader = (props: EbookProps) =>
 {
-    if(props.fileType === FileType.epub) {
-        return <EpubReader></EpubReader>
-    }
-    if(props.fileType === FileType.pdf) {
-        return <PdfReader></PdfReader>
-    }
+    const renderItem = determineReaderType(props)
+
+    return(
+        renderItem
+    )
 }
 
 export default EbookReader
--- src/components/PdfReader.tsx
+++ src/components/PdfReader.tsx
@@ -1,10 +1,38 @@
-import React from "react"
-import { View } from "react-native"
+import React, { useState } from "react"
+import { View } from "react-native" 
+import { PdfStyles } from "../ui/styles"
+import Pdf from 'react-native-pdf'
+import { Book } from "../types"
 
-const PdfReader = () =>
+var RNFS = require('react-native-fs');
+
+type PdfReaderProps = {
+    book: Book,
+}
+
+const PdfReader = (props: PdfReaderProps) =>
 {
+    const source = { uri: props.book.path }
+
     return (
-        <View></View>
+        <View style={PdfStyles.container}>
+            <Pdf
+                style={PdfStyles.pdf}
+                source={source}
+                onLoadComplete={(numberOfPages,filePath) => {
+                    console.log(`Number of pages: ${numberOfPages}`);
+                }}
+                onPageChanged={(page,numberOfPages) => {
+                    console.log(`Current page: ${page}`);
+                }}
+                onError={(error) => {
+                    console.log('error: ' + error);
+                }}
+                onPressLink={(uri) => {
+                    console.log(`Link pressed: ${uri}`);
+                }}
+                onLoadProgress={(progress) => {console.log('progress: ' + progress)}}/>
+        </View>
     )
 }
 
